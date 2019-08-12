86e61add452dbb7fff51969a3aa4c60bebc321ca
Royce <rosc1351@colorado.edu>
2019-08-12 14:17:17 -0600

    labeling axes and rotation

--- driver.py
+++ driver.py
@@ -4,8 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FFMpegWriter
 
-x = np.arange(-5,4,.4)
-y = np.arange(-4,5,.4)
+x = np.arange(-5,4,.3)
+y = np.arange(-4,5,.3)
 xs, ys = np.meshgrid(x, y)
 # z = calculate_R(xs, ys)
 zs = np.cos(xs) + np.cos(ys)
@@ -18,9 +18,13 @@ color = 0.5*np.ones((m,n))
 minn, maxx = 0,1.5
 
 fig = plt.figure()
-plot.plot(zs,color,minn,maxx,0)
-
-for i in range(100):
+angle = 0.0
+plot.plot(zs,color,minn,maxx,0,angle)
+angle += 1
+for i in range(255):
     print(i)
-    color = simulate.step(zs,color,1,.2)
-    plot.plot(zs,color,minn,maxx,i+1)
+    color = simulate.step(zs,color,1,.1)
+    color = simulate.step(zs,color,1,.1)
+    color = simulate.step(zs,color,1,.1)
+    plot.plot(zs,color,minn,maxx,i+1,angle)
+    angle += 1
--- plot.py
+++ plot.py
@@ -3,9 +3,10 @@ import matplotlib
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d.axes3d import Axes3D
 
-fig = plt.figure()
 
-def plot(z,color,minn,maxx,i):
+
+def plot(z,color,minn,maxx,i,angle):
+    fig = plt.figure()
     m,n = z.shape
     X = np.linspace(0,m,m)
     Y = np.linspace(0,n,n)
@@ -19,10 +20,19 @@ def plot(z,color,minn,maxx,i):
     ax = fig.add_subplot(1, 2, 1, projection='3d')
     ax.plot_surface(x, y, z, rstride=1, cstride=1, facecolors=fcolors, vmin=minn, vmax=maxx, shade=True)
     ax.title.set_text('Elevation map')
+    ax.view_init(30, angle)
+    ax.set_xlabel('Latitude')
+    ax.set_ylabel('Longitude')
+    ax.set_zlabel('Elevation')
 
     ax = fig.add_subplot(1, 2, 2, projection='3d')
     ax.plot_surface(x, y, z+color, rstride=1, cstride=1, facecolors=fcolors, vmin=minn, vmax=maxx, shade=True)
     ax.title.set_text('Elevation + Rain Level')
+    ax.view_init(30,angle)
+    ax.set_xlabel('Latitude')
+    ax.set_ylabel('Longitude')
+    ax.set_zlabel('Elevation')
     ax.set_zlim3d(-1,2)
 
-    fig.savefig('testSaveFig/'+str(i))
+    fig.savefig('PlotFrameImgs/'+str(i))
+    plt.close(fig)
